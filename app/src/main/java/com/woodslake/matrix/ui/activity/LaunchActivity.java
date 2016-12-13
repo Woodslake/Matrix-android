f8dc293d5c5f7373273b809bb0d8a494f6f144b6
Woodslake <woodslakelee@gmail.com>
2016-12-13 16:54:00 +0800

    ...

--- app/src/main/java/com/woodslake/matrix/ui/activity/LaunchActivity.java
+++ app/src/main/java/com/woodslake/matrix/ui/activity/LaunchActivity.java
@@ -1,16 +1,14 @@
 package com.woodslake.matrix.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.woodslake.matrix.R;
 import com.woodslake.matrix.base.ui.BaseActivity;
 import com.woodslake.matrix.engine.LaunchEngine;
+import com.woodslake.matrix.util.LogUtil;
 
 /**
  * Created by Woodslake on 2016/12/13.
@@ -21,8 +19,8 @@ public class LaunchActivity extends BaseActivity {
     private LaunchEngine mLaunchEngine;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
         initVariables(savedInstanceState);
         initView(savedInstanceState);
@@ -30,6 +28,7 @@ public class LaunchActivity extends BaseActivity {
 
     @Override
     protected void initVariables(Bundle savedInstanceState) {
+        LogUtil.i(TAG, "initVariables");
         mLaunchEngine = LaunchEngine.createInstance();
     }
 
